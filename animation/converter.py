6f1feff35f47f80eeba5f8af763b4df94847945b
Pinapelz <yukais@pinapelz.com>
2025-05-13 23:00:28 -0700

    adjust to dynamically calculate framecount

--- animation/converter.py
+++ animation/converter.py
@@ -1,8 +1,9 @@
-import numpy as np 
+import numpy as np
 import cv2
+import glob
 
 WIDTH = 44
-HEIGHT = 2 * int(0.43 * WIDTH) 
+HEIGHT = 2 * int(0.43 * WIDTH)
 
 POS = "\033[{};{}H"
 FG = "\033[38;2;{};{};{}m"
@@ -15,7 +16,7 @@ is_transparent = lambda clr1, clr2: clr1[3] == 0 and clr2[3] == 0
 def bgr_to_console(clr):
     result = np.array((0, 0, 0))
     alpha = clr[3] / 255.0
-    result = (alpha * clr[:3] + (1 - alpha) * result).astype(np.uint8) 
+    result = (alpha * clr[:3] + (1 - alpha) * result).astype(np.uint8)
     return result[::-1]
 
 def convert_frame(frame_path):
@@ -23,7 +24,7 @@ def convert_frame(frame_path):
     image = cv2.resize(image, dsize=(WIDTH, HEIGHT), interpolation=cv2.INTER_CUBIC)
     result = ""
     lc1, lc2 = None, None
-    for y in range(0, HEIGHT, 2): 
+    for y in range(0, HEIGHT, 2):
         result += POS.format(y // 2 + 1, 1)
         for x in range(WIDTH):
             if is_transparent(image[y + 1, x], image[y, x]):
@@ -38,7 +39,7 @@ def convert_frame(frame_path):
                 result += FG.format(*c1)
                 lc1 = c1
             if lc2 is None or not (c2 == lc2).all():
-                result += BG.format(*c2)            
+                result += BG.format(*c2)
                 lc2 = c2
             result += HB
     return result
@@ -49,14 +50,13 @@ print(convert_frame("./images/frame01.png"))
 #"""
 
 #"""
-frame_paths = [f"./images/frame{i:02}.png" for i in range(1, 40)]
-
+frame_paths = sorted(glob.glob("./images/frame*.png"))
 cfile = open("../jorb.c", "w")
 
 cfile.write('#include "jfetch.h"\n\n')
 cfile.write('static animation_object jorb = {\n')
 cfile.write('    .current_frame = 0,\n')
-cfile.write('    .frame_count = 39,\n')
+cfile.write(f'    .frame_count = {len(frame_paths)},\n')
 cfile.write('    .frames = {\n')
 
 max_len = 0
@@ -64,7 +64,7 @@ for fp in frame_paths:
     frame = repr(convert_frame(fp)).replace("'", '"')
     cfile.write(8 * ' ' + frame + ',\n')
     max_len = max(max_len, len(frame))
-    
+
 cfile.write('    }\n')
 cfile.write('};\n')
 cfile.close()
